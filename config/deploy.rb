b739b1bdee73701062d2031d496a93e55135897e
Andre Vidic <andre@dtcorp.com.au>
2016-08-06 19:05:04 +1000

    temporarily created new ssh key on server

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.6.0'
 
 set :application, 'parici.com.au'
-set :repo_url, 'https://github.com/drej2k/refinery_test_deploy.git'
+set :repo_url, 'git@github.com:drej2k/refinery_test_deploy.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
